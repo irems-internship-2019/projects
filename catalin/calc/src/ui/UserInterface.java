6f25338a3924f30f3d8e83d4695714270ca514d6
catalin1998 <catalinmorar98@yahoo.com>
2019-07-29 00:57:26 +0300

    update

--- catalin/calc/src/ui/UserInterface.java
+++ catalin/calc/src/ui/UserInterface.java
@@ -45,12 +45,11 @@ public class UserInterface {
 		shell.setLayout(new FillLayout());
 
 		Composite composite = new Composite(shell, SWT.NONE);
-		GridLayout layout = new GridLayout(4, true);
 
 		Text text = new Text(composite, SWT.BORDER);
 		text.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 4, 1));
 
-		composite.setLayout(layout);
+		composite.setLayout(new GridLayout(4, true));
 
 		createButtonsUI(text, composite);
 
